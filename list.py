6ef05fb6a528a362cf3eea8e2a5535d0a572a4a6
Thanasis Papamarinos <thanasis.papamarinos@gmail.com>
2018-01-10 13:26:51 +0200

    clean up code

--- list.py
+++ list.py
@@ -5,15 +5,13 @@ import re
 
 parser = argparse.ArgumentParser(description='Something')
 
-parser.add_argument('-a', '--action', type=str, help='The action to perform', required=True,
-    choices=["create", "delete", "list", "read"], dest="action", action="store")
+parser.add_argument('-a', '--action', type=str, help='The action to perform', required=True,choices=["create", "delete", "list", "read"], dest="action", action="store")
 parser.add_argument('-i', '--id', type=int, help='The id of the user', required=False, dest="Id", action="store")
 parser.add_argument('-e', '--email', type=str, help='The email of the user', required=False, dest="email", action="store")
 parser.add_argument('-f', '--first-name', type=str, help='The first name of the user', required=False, dest="firstName", action="store")
 parser.add_argument('-l', '--last-name', type=str, help='The last name of the user', required=False, dest="lastName", action="store")
 parser.add_argument('--file', type=str, help='The name of the file', required=False, default='list.txt', dest="file", action="store")
 args = parser.parse_args()
-
 
 # Util methods for reading file
 def getLinePart(line, part):
@@ -77,14 +75,11 @@ def delete(file, userId):
             f.write(user)
             f.write("\n")
 
-
 def list(file):
     with open(file,"r")as f:
         for line in f:
             printUser(line)
             
-    
-
 def read(file, userId):
     with open(file,"r")as f:
         for line in f:
@@ -96,17 +91,11 @@ def read(file, userId):
     
     print "User with id: %s not found\n" %userId
 
-
-
-
-
-
 # MAIN CODE
 if (os.path.isfile(args.file)):
     print ("file exists")
 else:
     print ("does not exist will be created")
-
 
 if (args.action == "create"):
     if (args.email == None or args.firstName == None or args.lastName == None):
